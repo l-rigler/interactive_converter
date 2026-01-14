c1b5fcf302adc6a9de3cb7cb2b073a7783ee451f
l-rigler <l.rigler@institut-myologie.com>
2026-01-14 11:12:56 +0100

    debugging of the loss (it's working with the nnunet pipeine now )

--- loss.py
+++ loss.py
@@ -1,7 +1,8 @@
 import torch
-from nnunetv2.training.loss.compound_losses.compound_lossses import DC_and_CE_loss
-from nnunetv2.training.loss.compound_losses.dice import SoftDiceLoss
+from nnunetv2.training.loss.compound_losses import DC_and_CE_loss
+from nnunetv2.training.loss.dice import SoftDiceLoss
 from torch.nn.functional import cross_entropy
+
 class region_loss(DC_and_CE_loss):
     def __init__(self, soft_dice_kwargs, ce_kwargs, weight_ce=1, weight_dice = 0, ignore_label=None,
                         dice_class = SoftDiceLoss):
@@ -12,22 +13,10 @@ class region_loss(DC_and_CE_loss):
         
     def forward(self,net_output,gt):
         self.click_map = self.click_map / self.click_map.mean()
-        loss = cross_entropy(net_output,gt,ignore_index = self.ignore_label,reduction = 'none')
+        loss = cross_entropy(net_output,gt.squeeze().long(),ignore_index = self.ignore_label,reduction = 'none')
         loss = loss * self.click_map
         loss = loss.mean()
-
-
-class click_pos_loss(DC_and_CE_loss):
-    def __init__(self, soft_dice_kwargs, ce_kwargs, weight_ce = 0, weight_dice=1, ignore_label=None,
-                        dice_class = SoftDiceLoss):
-            
-        super().__init__(soft_dice_kwargs, ce_kwargs, weight_ce, weight_dice, ignore_label,
-                        dice_class)
-        self.click_list = None
-
-    def forward(self,net_output,gt):
-        pass
-
+        return loss
 
 class combined_loss(DC_and_CE_loss):
     def __init__(self, soft_dice_kwargs, ce_kwargs, weight_ce=1, weight_dice=1, ignore_label=None,
@@ -36,7 +25,9 @@ class combined_loss(DC_and_CE_loss):
         super().__init__(soft_dice_kwargs, ce_kwargs, weight_ce, weight_dice, ignore_label,
                         dice_class)
         self.click_map = None
-
+        self.r_loss = region_loss( soft_dice_kwargs, ce_kwargs,ignore_label = ignore_label
+                        )
+        
     def forward(self,net_output,gt):
 
         if self.click_map==None or self.click_map.sum() == 0:
@@ -47,7 +38,9 @@ class combined_loss(DC_and_CE_loss):
             is_seg = 1 - (is_ignored.int().unsqueeze(2).unsqueeze(2))
             self.click_map = self.click_map * is_seg
             gt_masked = torch.where(self.click_map.unsqueeze(1) > 0,gt,self.ignore_label)
-
+            self.r_loss.click_map = self.click_map
+            return 0.5 * super().forward(net_output,gt) + 0.5 * self.r_loss.forward(net_output,gt_masked)
+        
 if __name__ == '__main__':
     gt = torch.randint(0,3(1,1,110,120,130))
     net_output0 = torch.rand((1,3,110,120,130))
