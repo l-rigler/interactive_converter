46c436b2edf3bdf6ab7c70cb468f78739099a05b
l-rigler <l.rigler@institut-myologie.com>
2026-01-14 15:44:52 +0100

    clean coding of the "modify the architecture func"

---
+++ utils.py
@@ -0,0 +1,42 @@
+import torch 
+import pathlib 
+import sys 
+
+
+def weight_grad_to_0(grad):
+    """freeze the weight of the first chan of a convolution"""
+    grad = grad.clone()
+    grad[:,0] = 0 
+    return grad
+
+def bias_grad_to_0(grad):
+    """freeze the bias of the first chan of a convolution"""
+    grad[0] = 0 
+    return grad
+
+def modify_network_for_click_in_training(network):
+    #TODO modify the hardcoded part to fit any network 
+
+    #retrieving the first conv of the encoder
+    first_layer = network.encoder.stages[0]
+    input_conv = first_layer[0].convs[0].conv   
+    
+    #building the new conv 
+    new_conv = torch.nn.Conv3d(10,32,kernel_size = (1,3,3),stride = (1,1,1), padding = (0,1,1)) # the dimension to change (for input channels) is the second one 
+    torch.nn.init.normal_(new_conv.weight,mean = 0, std = 0.001)
+    torch.nn.init.constant_(new_conv.bias,0)
+    with torch.no_grad():
+        new_conv.weight[:,0] = input_conv.weight[:,0]
+        network.encoder.stages[0][0].convs[0].conv = new_conv
+    
+    #freezing the encoder part for the training ! 
+    for stage in network.encoder.stages[1:]: 
+        for param in stage.parameters():
+            param.requires_grad = False
+    # stage 0 has to be done manually 
+    for param in network.encoder.stages[0][0].convs[1].parameters():
+        param.requires_grad = False
+    
+    # freezing the first channel of the first conv 
+    network.encoder.stages[0][0].convs[0].conv.weight.register_hook(weight_grad_to_0)
+    network.encoder.stages[0][0].convs[0].conv.bias.register_hook(bias_grad_to_0)
